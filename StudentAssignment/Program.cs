9542546c87c5509a61ad19394871472885509909
CataMarchis <marchiscatalin@yahoo.com>
2023-08-17 22:07:12 +0300

    Add project files.

---
+++ StudentAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using StudentAssignment;
+
+Console.Write("Student name and age: ");
+string a = Console.ReadLine();
+int b = Convert.ToInt32(Console.ReadLine());
+Student student = new Student(a, b);
+Console.WriteLine(student.Info);
+
+Console.Write("Student name and age: ");
+string c = Console.ReadLine();
+int d = Convert.ToInt32(Console.ReadLine());
+Student student1 = new Student(c, d);
+Console.WriteLine(student1.Info);
+
+Console.Write("Student name and age: ");
+string x = Console.ReadLine();
+int y = Convert.ToInt32(Console.ReadLine());
+Student student2 = new Student(x, y);
+Console.WriteLine(student2.Info);
+
+Console.WriteLine(" {0} has the following mark: ", student.name);
+student.Mark = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" {0} has the following mark: ", student1.name);
+student1.Mark = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("The average mark of our students is: ");
+student2.averageMarks();
+
+/*
+
+int nrOfstudents = 3;
+
+for(int i = 0; i < nrOfstudents; i++)
+{
+    Console.Write("Student name and age: ");
+    string a = Console.ReadLine();
+    int b = Convert.ToInt32(Console.ReadLine());
+    Student student = new Student(a, b);
+    Console.WriteLine(student.Info);
+    Console.Write(" {0} has the following mark: ", student.name);
+    student.Mark = Convert.ToDouble(Console.ReadLine());
+    if(i == nrOfstudents - 1)
+    {
+        Console.WriteLine("The average mark of our students is: ");
+        student.averageMarks(); 
+    }
+}
+
+*/
---
+++ StudentAssignment/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAssignment
+{
+    class Student
+    {
+        public string name;
+        public int age;
+        const int MIN_AGE = 18;
+        const int MAX_AGE = 99;
+        public double mark = 0;
+        static double sumOfMarks;
+        static int nrOfMarks = 0;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < MIN_AGE || value > MAX_AGE) 
+                {
+                    Console.WriteLine("Invalid age value!");
+                }
+                else age = value;
+            }
+        }
+        public Student(string name, int age) 
+        {
+            this.name = name;
+            Age = age;
+        }
+        public string Info
+        {
+            get
+            {
+                if (Age < MAX_AGE && age > MIN_AGE)
+                {
+                    return name + Convert.ToString(Age);
+                }
+                return name;
+            }
+
+        }
+        public double Mark
+        {
+            set
+            {
+                if (value > 0 && value <= 10)
+                {
+                    mark = value;
+                    sumOfMarks = sumOfMarks + value;
+                    nrOfMarks++;
+                }
+                else if (value > 10 || value < 0)
+                    Console.WriteLine("Invalid mark!");
+            } 
+        } 
+        public void averageMarks()
+        {
+            Console.WriteLine(sumOfMarks/nrOfMarks);
+        }
+    }
+}
